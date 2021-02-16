258f9d4fd659fb626a558fbc4b69602203323b92
nndjov <66019531+nndjov@users.noreply.github.com>
2021-02-16 16:54:27 +0100

    Delete ClassLoaderTest.java

--- 1_Izvrsavanja/Primeri/1_ClassLoader/ClassLoaderTest.java
+++
@@ -1,8 +0,0 @@
-public class ClassLoaderTest extends JavaClassLoader {
-
-	public static void main(String[] args) {
-
-		JavaClassLoader jcl = new JavaClassLoader();
-		jcl.pozoviMetodu("MojaKlasa", "kaziZdravo");
-	}
-}
